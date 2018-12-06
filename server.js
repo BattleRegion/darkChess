7e0645eaeb47b4968ec9802a4dacfbf63f02f51f
刘佳黎 <lizi@liujialideMacBook-Pro.local>
2018-12-06 14:59:50 +0800

    first

---
+++ server.js
@@ -0,0 +1,33 @@
+const WebSocket = require('ws');
+const Log = require('./util/log');
+const port = 8888;
+const server_opts = {
+    port: port
+};
+const webSocketServer = new WebSocket.Server(server_opts);
+
+Log.debug(`server listen ${port}`);
+
+webSocketServer.on("connection", (ws, req)=>{
+    Log.debug(`ws client ${ws._socket['remoteAddress']}:${ws._socket['remotePort']} connected`);
+    ws.on("message", data=>{
+        Log.debug(`ws client receive msg ${data}`);
+    });
+
+    ws.on("close", (code, reason)=>{
+        Log.debug(`ws client close code ${code} reason ${reason}`);
+    });
+
+    ws.on("error", e=>{
+        Log.debug(`ws client error ${e}`);
+    });
+});
+
+webSocketServer.on("error", error=>{
+    Log.debug(`ws server error ${error}`);
+});
+
+webSocketServer.on("close", ()=>{
+    Log.debug(`ws server close`);
+
+});
